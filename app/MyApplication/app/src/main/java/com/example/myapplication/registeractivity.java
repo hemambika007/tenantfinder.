01b6852b0d436eecfdf538e7569c117825f2e407
Hemambika <hemambikabanda071@gmail.com>
2022-03-30 01:04:39 +0530

    loginactivity

--- app/MyApplication/app/src/main/java/com/example/myapplication/registeractivity.java
+++ app/MyApplication/app/src/main/java/com/example/myapplication/registeractivity.java
@@ -8,6 +8,6 @@ public class Signupactivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signupactivity);
+        setContentView(R.layout.activity_registeractivity);
     }
 }
