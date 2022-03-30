f9f7ec18e31f11bb806859e0ab824b5cda03d922
Hemambika <hemambikabanda071@gmail.com>
2022-03-30 10:40:09 +0530

    loginactivity

--- app/MyApplication/app/src/main/java/com/example/myapplication/registeractivity.java
+++ app/MyApplication/app/src/main/java/com/example/myapplication/registeractivity.java
@@ -3,7 +3,7 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Signupactivity extends AppCompatActivity {
+public class registeractivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
