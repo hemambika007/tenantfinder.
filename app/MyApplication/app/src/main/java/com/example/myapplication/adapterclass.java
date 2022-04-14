e108b3c8659e1644ba33876557dc4bbac285e4b3
Hemambika <hemambikabanda071@gmail.com>
2022-04-15 00:20:06 +0530

    loginactivity

---
+++ app/MyApplication/app/src/main/java/com/example/myapplication/adapterclass.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class adapterclass extends RecyclerView.Adapter<adapterclass.MyViewHolder> {
+
+    private List<dataclass> PropertyList;
+
+    public adapterclass (List<dataclass>PropertyList){ this .PropertyList=PropertyList; }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.page_design,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        int profile= PropertyList.get(position).getImageview1();
+        String username= PropertyList.get(position).getTextview();
+        String location= PropertyList.get(position).getTextview2();
+        String Property= PropertyList.get(position).getTextview3();
+        String mobile= PropertyList.get(position).getTextview4();
+        String divider= PropertyList.get(position).getDivider();
+
+         holder.updateui(profile,username,location,Property,mobile,divider);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return PropertyList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+         ImageView imageView;
+         TextView textView;
+         TextView textView2;
+         TextView textView3;
+         TextView textView4;
+         TextView divider;
+
+        public MyViewHolder(@NonNull View view) {
+            super(view);
+
+            imageView= view.findViewById(R.id.imageview1);
+            textView= view.findViewById(R.id.textView);
+            textView2= view.findViewById(R.id.textView2);
+            textView3= view.findViewById(R.id.textView3);
+            textView4= view.findViewById(R.id.textView4);
+            divider= view.findViewById(R.id.divider);
+        }
+
+        public void updateui(int profile, String username, String location, String property, String mobile, String divider) {
+            //write the remaining code here!
+        }
+    }
+}
---
+++ app/MyApplication/app/src/main/java/com/example/myapplication/dataclass.java
@@ -0,0 +1,69 @@
+package com.example.myapplication;
+
+public class dataclass {
+
+    private int imageview1;
+    private String textview;
+    private String textview2;
+    private String textview3;
+    private String textview4;
+    private String divider;
+
+    public dataclass(){}
+    public dataclass(int imageview1,String textview,String textview2 , String textview3, String textview4, String divider) {
+        this.imageview1 = imageview1;
+        this.textview = textview;
+        this.textview2 = textview2;
+        this.textview3 = textview3;
+        this.textview4 = textview4;
+        this.divider = divider;
+    }
+
+    public int getImageview1() {
+        return imageview1;
+    }
+
+    public void setImageview1(int imageview1) {
+        this.imageview1 = imageview1;
+    }
+
+    public String getTextview() {
+        return textview;
+    }
+
+    public void setTextview(String textview) {
+        this.textview = textview;
+    }
+
+    public String getTextview2() {
+        return textview2;
+    }
+
+    public void setTextview2(String textview2) {
+        this.textview2 = textview2;
+    }
+
+    public String getTextview3() {
+        return textview3;
+    }
+
+    public void setTextview3(String textview3) {
+        this.textview3 = textview3;
+    }
+
+    public String getTextview4() {
+        return textview4;
+    }
+
+    public void setTextview4(String textview4) {
+        this.textview4 = textview4;
+    }
+
+    public String getDivider() {
+        return divider;
+    }
+
+    public void setDivider(String divider) {
+        this.divider = divider;
+    }
+}
